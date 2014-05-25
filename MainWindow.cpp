ab7286c93fad9a863957ae39666a244fd6def8b1
Jason Champion <jchampion@zetacentauri.com>
2014-05-25 12:58:32 -0700

    Rearrange buttons and make enter handler for search and address bars work.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -48,77 +48,81 @@ MainWindow::MainWindow( )
     QHBoxLayout* topRowLayout = new QHBoxLayout();
     rootLayout->addLayout(topRowLayout);
 
-    _txtURL = new QLineEdit( this );
-    topRowLayout->addWidget(_txtURL);
-
-    _btnGo = new QPushButton( this );
-    _btnGo->setIcon(QPixmap(play_xpm));
-    _btnGo->setToolTip("Go.");
-    connect(_btnGo, SIGNAL(released()), this, SLOT(OnButtonGoClick()));
-    topRowLayout->addWidget(_btnGo);
-
-    _txtSearch = new QLineEdit( this );
-    topRowLayout->addWidget(_txtSearch);
-
-    _btnSearch = new QPushButton( this );
-    _btnSearch->setIcon(QPixmap(play_xpm));
-    _btnSearch->setToolTip("Search.");
-    connect(_btnSearch, SIGNAL(released()), this, SLOT(OnButtonSearchClick()));
-    topRowLayout->addWidget(_btnSearch);
-
-    QHBoxLayout* firstRowLayout = new QHBoxLayout();
-    rootLayout->addLayout(firstRowLayout);
-
-    firstRowLayout->setContentsMargins(QMargins(0,0,0,0));
-    firstRowLayout->setSpacing(2);
-
-    _btnLarger = new QPushButton( this );
-    _btnLarger->setIcon(QPixmap(add_xpm));
-    _btnLarger->setToolTip("Larger page");
-    connect(_btnLarger, SIGNAL(released()), this, SLOT(OnButtonLargerClick()));
-    firstRowLayout->addWidget(_btnLarger);
-
-    _btnSmaller = new QPushButton( this );
-    _btnSmaller->setIcon(QPixmap(remove_xpm));
-    _btnSmaller->setToolTip("Smaller page.");
-    connect(_btnSmaller, SIGNAL(released()), this, SLOT(OnButtonSmallerClick()));
-    firstRowLayout->addWidget(_btnSmaller);
-
-    _btnHome = new QPushButton( this );
-    _btnHome->setIcon(QPixmap(stop_xpm));
-    _btnHome->setToolTip("Home.");
-    connect(_btnHome, SIGNAL(released()), this, SLOT(OnButtonHomeClick()));
-    firstRowLayout->addWidget(_btnHome);
-
     _btnBack = new QPushButton( this );
     _btnBack->setIcon(QPixmap(reverse_xpm));
     _btnBack->setToolTip("Previous page.");
     connect(_btnBack, SIGNAL(released()), this, SLOT(OnButtonBackClick()));
-    firstRowLayout->addWidget(_btnBack);
+    topRowLayout->addWidget(_btnBack);
 
-    _btnReload = new QPushButton( this );
-    _btnReload->setIcon(QPixmap(reverse_xpm));
-    _btnReload->setToolTip("Reload page.");
-    connect(_btnReload, SIGNAL(released()), this, SLOT(OnButtonReloadClick()));
-    firstRowLayout->addWidget(_btnReload);
-
-	_btnStop = new QPushButton( this );
+    _btnStop = new QPushButton( this );
     _btnStop->setIcon(QPixmap(stop_xpm));
-	_btnStop->setToolTip("Stop.");
+    _btnStop->setToolTip("Stop.");
     connect(_btnStop, SIGNAL(released()), this, SLOT(OnButtonStopClick()));
-    firstRowLayout->addWidget(_btnStop);
+    topRowLayout->addWidget(_btnStop);
 
     _btnForward = new QPushButton( this );
     _btnForward->setIcon(QPixmap(play_xpm));
     _btnForward->setToolTip("Forward.");
     connect(_btnForward, SIGNAL(released()), this, SLOT(OnButtonForwardClick()));
-    firstRowLayout->addWidget(_btnForward);
+    topRowLayout->addWidget(_btnForward);
+
+    _txtURL = new QLineEdit( this );
+    connect(_txtURL, SIGNAL(returnPressed()), this, SLOT(OnButtonGoClick()));
+    topRowLayout->addWidget(_txtURL);
+
+    //_btnGo = new QPushButton( this );
+    //_btnGo->setIcon(QPixmap(play_xpm));
+    //_btnGo->setToolTip("Go.");
+    //connect(_btnGo, SIGNAL(released()), this, SLOT(OnButtonGoClick()));
+    //topRowLayout->addWidget(_btnGo);
+
+    _txtSearch = new QLineEdit( this );
+    _txtSearch->setMaximumWidth(200);
+    _txtSearch->setFixedWidth(200);
+    connect(_txtSearch, SIGNAL(returnPressed()), this, SLOT(OnButtonSearchClick()));
+    topRowLayout->addWidget(_txtSearch);
+
+    //_btnSearch = new QPushButton( this );
+    //_btnSearch->setIcon(QPixmap(play_xpm));
+    //_btnSearch->setToolTip("Search.");
+    //connect(_btnSearch, SIGNAL(released()), this, SLOT(OnButtonSearchClick()));
+    //topRowLayout->addWidget(_btnSearch);
+
+    //QHBoxLayout* firstRowLayout = new QHBoxLayout();
+    //rootLayout->addLayout(firstRowLayout);
+
+    //firstRowLayout->setContentsMargins(QMargins(0,0,0,0));
+    //firstRowLayout->setSpacing(2);
+
+    _btnLarger = new QPushButton( this );
+    _btnLarger->setIcon(QPixmap(add_xpm));
+    _btnLarger->setToolTip("Larger page");
+    connect(_btnLarger, SIGNAL(released()), this, SLOT(OnButtonLargerClick()));
+    topRowLayout->addWidget(_btnLarger);
+
+    _btnSmaller = new QPushButton( this );
+    _btnSmaller->setIcon(QPixmap(remove_xpm));
+    _btnSmaller->setToolTip("Smaller page.");
+    connect(_btnSmaller, SIGNAL(released()), this, SLOT(OnButtonSmallerClick()));
+    topRowLayout->addWidget(_btnSmaller);
+
+    _btnHome = new QPushButton( this );
+    _btnHome->setIcon(QPixmap(stop_xpm));
+    _btnHome->setToolTip("Home.");
+    connect(_btnHome, SIGNAL(released()), this, SLOT(OnButtonHomeClick()));
+    topRowLayout->addWidget(_btnHome);
+
+    _btnReload = new QPushButton( this );
+    _btnReload->setIcon(QPixmap(reverse_xpm));
+    _btnReload->setToolTip("Reload page.");
+    connect(_btnReload, SIGNAL(released()), this, SLOT(OnButtonReloadClick()));
+    topRowLayout->addWidget(_btnReload);
 
 	_btnAbout = new QPushButton( this );
     _btnAbout->setIcon(QPixmap(question_xpm));
 	_btnAbout->setToolTip("About Tikbew.");
     connect(_btnAbout, SIGNAL(released()), this, SLOT(OnAbout()));
-    firstRowLayout->addWidget(_btnAbout);
+    topRowLayout->addWidget(_btnAbout);
 
     QHBoxLayout* secondRowLayout = new QHBoxLayout();
     rootLayout->addLayout(secondRowLayout);
@@ -129,12 +133,19 @@ MainWindow::MainWindow( )
 
     connect(_browser, SIGNAL(urlChanged(QUrl)), this, SLOT(UpdateUrl()));
     connect(_browser, SIGNAL(titleChanged(QString)), this, SLOT(UpdateTitle()));
-    connect(_txtURL, SIGNAL(returnPressed()), _btnGo, SIGNAL(clicked()));
-    connect(_txtSearch, SIGNAL(returnPressed()), _btnSearch, SIGNAL(clicked()));
+    //connect(_txtURL, SIGNAL(returnPressed()), _btnGo, SIGNAL(clicked()));
+    //connect(_txtSearch, SIGNAL(returnPressed()), _btnSearch, SIGNAL(clicked()));
 
     QIcon icon = QIcon(QPixmap(TikBew32_xpm));
     setWindowIcon(icon);
     resize(QSize(1024, 768));
+    _txtSearch->setFocus();
+    QList<QPushButton*> buttonList = findChildren<QPushButton*>();
+    foreach(QPushButton* button, buttonList)
+    {
+        button->setDefault(false);
+        button->setAutoDefault(false);
+    }
 	LoadSettings();
 }
 
@@ -145,7 +156,7 @@ void MainWindow::UpdateUrl()
 
 void MainWindow::UpdateTitle()
 {
-    setWindowTitle(_browser->title() + QString("- TikBew"));
+    setWindowTitle(_browser->title() + QString(" - TikBew"));
 }
 
 void MainWindow::LoadSettings()
@@ -215,10 +226,10 @@ void MainWindow::OnAbout()
 {
 #ifdef WIN32
     QMessageBox::about(this, QString("TikBew ") + QString(VERSION_STRING),
-                       QString("TikBew ") + QString(VERSION_STRING) + QString("\nCopyright 2014 Zeta Centauri.\nDeveloped by Jason Champion.\nTikBew is free software and may be distributed freely.\nhttp://tikbew.com\nTikBew uses the Qt libraries."));
+                       QString("TikBew ") + QString(VERSION_STRING) + QString("\nCopyright 2014 Championix, LLC.\nDeveloped by Jason Champion.\nTikBew is free software and may be distributed freely.\nhttp://tikbew.com\nTikBew uses the Qt libraries."));
 #else
     QMessageBox::about(this, QString("TikBew ") + QString(VERSION_STRING),
-                       QString("TikBew ") + QString(VERSION_STRING) + QString("\nCopyright 2014 Zeta Centauri.\nDeveloped by Jason Champion.\nTikBew is free software and may be distributed freely.\nhttp://tikbew.com\nTikBew uses the Qt libraries."));
+                       QString("TikBew ") + QString(VERSION_STRING) + QString("\nCopyright 2014 Championix, LLC.\nDeveloped by Jason Champion.\nTikBew is free software and may be distributed freely.\nhttp://tikbew.com\nTikBew uses the Qt libraries."));
 #endif
 }
 
